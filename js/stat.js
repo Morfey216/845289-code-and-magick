9b1af05cbe7efc04c26d961edaf7572cdb457ef6
Morfey216 <Morfey4web@gmail.com>
2018-11-25 14:04:48 +0300

    Построение простого облака

---
+++ js/stat.js
@@ -0,0 +1,15 @@
+var CLOUD_WIDTH = 500;
+var CLOUD_HEIGHT = 280;
+var CLOUD_X = 100;
+var CLOUD_Y = 0;
+var GAP = 10;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function(ctx) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+};
