1c853f21e6bd96d50ba18c016943ddabb73ebda2
Morfey216 <Morfey4web@gmail.com>
2018-11-25 16:41:40 +0300

    Правка ошибок Тревиса

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CLOUD_WIDTH = 500;
 var CLOUD_HEIGHT = 280;
 var CLOUD_X = 100;
@@ -5,19 +7,18 @@ var CLOUD_Y = 0;
 var GAP = 10;
 var FONT_GAP = 15;
 var TIME_GAP = 20;
-var TEXT_WIDTH = 80;
 var COLUMN_WIDTH = 70;
 var barHeight = CLOUD_HEIGHT - (GAP + FONT_GAP + GAP) * 2 - GAP;
 // var barWidth = CLOUD_WIDTH - (COLUMN_WIDTH + GAP) * 2;
 var barPositionX = CLOUD_X + COLUMN_WIDTH + GAP;
 var barPositionY = CLOUD_Y + GAP + FONT_GAP + GAP + GAP / 2;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -29,7 +30,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
