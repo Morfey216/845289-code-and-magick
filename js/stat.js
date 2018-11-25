100c44d179f32ea3ac916e9f955e20fdb5f9ae76
Morfey216 <Morfey4web@gmail.com>
2018-11-25 14:27:33 +0300

    Вывод сообщения о победе

--- js/stat.js
+++ js/stat.js
@@ -3,13 +3,18 @@ var CLOUD_HEIGHT = 280;
 var CLOUD_X = 100;
 var CLOUD_Y = 0;
 var GAP = 10;
+var FONT_GAP = 15;
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx) {
+window.renderStatistics = function(ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#00a';
+  ctx.textAlign = 'center';
+  ctx.fillText('Поздравляем! Вы сожгли забор!', CLOUD_WIDTH / 2 + CLOUD_X, CLOUD_Y + GAP + FONT_GAP);
 };
