e3323b05369706af044f0bf79200c9e424efa82a
Morfey216 <Morfey4web@gmail.com>
2018-12-08 13:00:07 +0300

    Добавление запрета закрытия окна настроек по ESC при активном поле ввода имени

--- js/setup.js
+++ js/setup.js
@@ -69,6 +69,7 @@ setupOpen.addEventListener('keydown', function (evt) {
 
 function openPopup() {
   var setupClose = userDialog.querySelector('.setup-close');
+  var userNameInput = userDialog.querySelector('.setup-user-name');
 
   userDialog.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
@@ -83,8 +84,29 @@ function openPopup() {
     }
   });
 
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  userNameInput.addEventListener('input', function (evt) {
+    var target = evt.target;
+    if (target.value.length < 2) {
+      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+
   function onPopupEscPress(evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
+    if (evt.keyCode === ESC_KEYCODE && document.activeElement !== userNameInput) {
       closePopup();
     }
   }
